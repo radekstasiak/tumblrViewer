876925fd0fee0b37c0719e1d51935bd8283529f7
radekstasiak <radek.stasiak@gmail.com>
2015-01-05 17:35:24 +0100

    new look

--- app/build/generated/source/r/debug/com/example/tumblrviewer/R.java
+++ app/build/generated/source/r/debug/com/example/tumblrviewer/R.java
@@ -11,8 +11,16 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int blue=0x7f050000;
-        public static final int turquise=0x7f050001;
+        public static final int black=0x7f050000;
+        public static final int brown=0x7f050001;
+        public static final int dark_slate_gray=0x7f050002;
+        public static final int gray_1=0x7f050003;
+        public static final int gray_2=0x7f050004;
+        public static final int light_brown=0x7f050005;
+        public static final int transparent=0x7f050006;
+        public static final int tumblrBlue=0x7f050007;
+        public static final int turquise=0x7f050008;
+        public static final int white=0x7f050009;
     }
     public static final class dimen {
         public static final int action_bar_default_height=0x7f060000;
@@ -21,21 +29,33 @@ public final class R {
         public static final int default_icon_size=0x7f060003;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int background_label=0x7f020001;
+        public static final int costum_progress_bar=0x7f020002;
+        public static final int custom_progress_bar=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int image=0x7f020005;
+        public static final int tumblr_logo=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0006;
-        public static final int items_lv=0x7f0a0000;
-        public static final int login_btn=0x7f0a0003;
-        public static final int password_et=0x7f0a0002;
-        public static final int tags_layout=0x7f0a0005;
-        public static final int tumblr_photo_iv=0x7f0a0004;
-        public static final int username_et=0x7f0a0001;
+        public static final int action_settings=0x7f0a000b;
+        public static final int cat=0x7f0a0000;
+        public static final int items_lv=0x7f0a0002;
+        public static final int load_more_footer=0x7f0a0008;
+        public static final int load_more_progressBar=0x7f0a000a;
+        public static final int login_btn=0x7f0a0005;
+        public static final int password_et=0x7f0a0004;
+        public static final int tags_layout=0x7f0a0007;
+        public static final int title_tv=0x7f0a0001;
+        public static final int top_line=0x7f0a0009;
+        public static final int tumblr_photo_iv=0x7f0a0006;
+        public static final int username_et=0x7f0a0003;
     }
     public static final class layout {
         public static final int activity_list_viewer=0x7f030000;
         public static final int activity_login=0x7f030001;
         public static final int item_on_list=0x7f030002;
+        public static final int load_more_footer=0x7f030003;
     }
     public static final class menu {
         public static final int list_viewer=0x7f090000;
@@ -48,7 +68,9 @@ public final class R {
         public static final int action_settings=0x7f070000;
         public static final int app_name=0x7f070001;
         public static final int hello_world=0x7f070002;
-        public static final int title_activity_list_viewer=0x7f070003;
+        public static final int tag_label=0x7f070003;
+        public static final int title=0x7f070004;
+        public static final int title_activity_list_viewer=0x7f070005;
     }
     public static final class style {
         /** 
